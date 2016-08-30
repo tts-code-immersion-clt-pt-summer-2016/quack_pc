31406870d7ad8e2b29b258af49c653431b5549eb
Shane Barringer <shane.barringer@outlook.com>
2016-08-30 09:59:11 -0400

    adds new devise method for updating permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   protected
 
-  def configure_devise_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation,
-      :name, :username, :bio, :location) }
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location, :email, :password, :password_confirmation])
 
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation,
-      :current_password, :name, :username, :bio, :location) }
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location, :email, :password, :password_confirmation])
   end
 end
